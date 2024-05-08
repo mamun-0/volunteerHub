caed23065dd455b2fcdf51e1d7583ef9974cbe7c
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-05-08 23:32:06 +0600

    router setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render("");
+import { router } from "../router";
+import { RouterProvider } from "react-router-dom";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}></RouterProvider>
+);
