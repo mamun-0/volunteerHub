4d5dbf31972d58d71e5cf1a424b65c2f8e050d46
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-05-09 11:10:03 +0600

    firebaseAuth hook

---
+++ hooks/useFirebaseAuth.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { auth } from "../config/firebase";
+import {
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+export function useFirebaseAuth(initialUser = null) {
+  const [user, setInitialUser] = useState(initialUser);
+
+  function setUser(user) {
+    setInitialUser(user);
+  }
+  function signUpWithEmailAndPassword(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+  function loginWithEmailAndPassword(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+  function updateUserProfile(displayName, photoURL) {
+    return updateProfile(auth.currentUser, {
+      displayName,
+      photoURL,
+    });
+  }
+  function loginWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider);
+  }
+  function loginWithGithub() {
+    const provider = new GithubAuthProvider();
+    return signInWithPopup(auth, provider);
+  }
+  function logOut() {
+    return signOut(auth);
+  }
+  return {
+    user,
+    setUser,
+    signUpWithEmailAndPassword,
+    loginWithEmailAndPassword,
+    updateUserProfile,
+    loginWithGoogle,
+    loginWithGithub,
+    logOut,
+  };
+}
