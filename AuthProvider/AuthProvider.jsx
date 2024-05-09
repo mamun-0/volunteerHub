15e278f034729d59c20ca9ae397af762d7d9bb15
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-05-09 11:09:14 +0600

    authProvider

---
+++ AuthProvider/AuthProvider.jsx
@@ -0,0 +1,34 @@
+import { createContext, useEffect } from "react";
+import { useFirebaseAuth } from "../hooks/useFirebaseAuth";
+import { useLoading } from "../hooks/useLoading";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../config/firebase";
+const AuthContext = createContext(null);
+
+export function AuthProvider({ children }) {
+  const firebaseAuth = useFirebaseAuth(null);
+  const [loading, setLoading] = useLoading(true);
+  useEffect(() => {
+    const { setUser } = firebaseAuth;
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+  return (
+    <AuthContext.Provider
+      value={{
+        firebaseAuth,
+        loading,
+        setLoading,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
