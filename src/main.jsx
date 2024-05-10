754ea99b4c3aee845c8d9c8a7096eed041e669cf
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-05-10 18:26:36 +0600

    add toast-container's css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { router } from "../router";
+import "react-toastify/dist/ReactToastify.css";
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "../AuthProvider/AuthProvider";
 ReactDOM.createRoot(document.getElementById("root")).render(
