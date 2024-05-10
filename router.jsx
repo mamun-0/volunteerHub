014d66e63d214826d23d2a21e7ecbd36569d1f88
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-05-10 18:25:31 +0600

    login and register route

--- router.jsx
+++ router.jsx
@@ -1,8 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Main } from "./src/components/Layout/Main";
+import { Login } from "./src/pages/Login/Login";
+import { Register } from "./src/pages/Register/Register";
 export const router = createBrowserRouter([
   {
     element: <Main />,
-    children: [{ path: "/", element: <h2>Home</h2> }],
+    children: [
+      { path: "/", element: <h2>Home</h2> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+    ],
   },
 ]);
