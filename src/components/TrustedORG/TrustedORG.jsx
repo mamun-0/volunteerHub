75a1865c9d9d1d0ae1a507b76cd4d9f6e72ba030
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-05-11 12:10:52 +0600

    Trusted Companies

---
+++ src/components/TrustedORG/TrustedORG.jsx
@@ -0,0 +1,23 @@
+import Marquee from "react-fast-marquee";
+import "./TrustedORG.css";
+export function TrustedORG() {
+  return (
+    <div className="image-space">
+      <Marquee pauseOnHover={true}>
+        <img src="/images/marquee/1.png" alt="" />
+        <img src="/images/marquee/2.png" alt="" />
+        <img src="/images/marquee/3.png" alt="" />
+        <img src="/images/marquee/4.png" alt="" />
+        <img src="/images/marquee/5.png" alt="" />
+        <img src="/images/marquee/6.png" alt="" />
+        <img src="/images/marquee/7.png" alt="" />
+        <img src="/images/marquee/8.png" alt="" />
+        <img src="/images/marquee/10.png" alt="" />
+        <img src="/images/marquee/11.webp" alt="" />
+        <img src="/images/marquee/12.png" alt="" />
+        <img src="/images/marquee/13.png" alt="" />
+        <img src="/images/marquee/14.png" alt="" />
+      </Marquee>
+    </div>
+  );
+}
