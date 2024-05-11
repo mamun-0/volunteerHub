9d65b67f1fd4789bba7bcf3feb0d4f9e7b9b1835
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-05-11 12:12:52 +0600

    Add home route

--- router.jsx
+++ router.jsx
@@ -3,12 +3,13 @@ import { Main } from "./src/components/Layout/Main";
 import { Login } from "./src/pages/Login/Login";
 import { Register } from "./src/pages/Register/Register";
 import { Error404 } from "./src/pages/Error/Error404";
+import { Home } from "./src/pages/Home/Home";
 export const router = createBrowserRouter([
   {
     element: <Main />,
     errorElement: <Error404 />,
     children: [
-      { path: "/", element: <h2>Home</h2> },
+      { path: "/", element: <Home /> },
       { path: "/login", element: <Login /> },
       { path: "/register", element: <Register /> },
     ],
