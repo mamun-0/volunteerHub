3d1a87e514da5a47b132477b30fbc83f82546db4
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-05-11 12:11:24 +0600

    Optimize home section part

---
+++ src/components/Optimize/Optimize.jsx
@@ -0,0 +1,21 @@
+export function Optimize() {
+  return (
+    <div className="flex">
+      <div className="w-1/2">
+        <img className="w-full" src="/images/second-section.webp" alt="" />
+      </div>
+      <div className="w-1/2 flex flex-col justify-center">
+        <h2 className="text-3xl font-medium text-sky-600">
+          Optimized for All Devices
+        </h2>
+        <p className="textarea-md text-slate-600">
+          VolunteerHub Landing Pages are designed and optimized to be responsive
+          and compatible with all devices including desktop computers, laptops,
+          tablets, and smartphones. Reach, engage, and recruit volunteers on any
+          device, from anywhere with an internet connection. Provide your
+          supporters a consistent experience.
+        </p>
+      </div>
+    </div>
+  );
+}
