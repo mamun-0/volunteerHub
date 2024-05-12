259ff0746f38b7d35bf435f87f96360d006f5ad8
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-05-13 00:45:49 +0600

    Home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { Banner } from "../../components/Banner/Banner";
+import { Optimize } from "../../components/Optimize/Optimize";
+import { SectionTitle } from "../../components/SectionTitle/SectionTitle";
+import { TrustedORG } from "../../components/TrustedORG/TrustedORG";
+import axios from "axios";
+import { LoadingSpin } from "../../components/LoadingSpin/LoadingSpin";
+import { UpcomingDateLine } from "../../components/UpcomingDeadLine/UpcomingDateLine";
+export function Home() {
+  const [deadLine, setDeadLine] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/need-volunteer-6`)
+      .then(({ data }) => {
+        setDeadLine(data.message);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <Banner />
+      <SectionTitle title={"Volunteer Needs Now"} size={3} />
+      <SectionTitle
+        title={
+          "Volunteer Management software trusted by thousands of organizations, including : "
+        }
+        size={1}
+      />
+      <TrustedORG />
+      <SectionTitle title={"Volunteer Needs Now"} size={3} />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-4">
+        {loading ? (
+          <LoadingSpin />
+        ) : (
+          deadLine.map((person, idx) => {
+            return <UpcomingDateLine key={idx} {...person} />;
+          })
+        )}
+      </div>
+      <SectionTitle title={"Customizable for Each Initiative"} size={3} />
+      <Optimize />
+    </div>
+  );
+}
