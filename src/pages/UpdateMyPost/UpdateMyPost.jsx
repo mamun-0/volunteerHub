a978b2086a3deabe6526891c68786f91f7f011f7
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-05-14 01:50:53 +0600

    UpdateMyPost page

---
+++ src/pages/UpdateMyPost/UpdateMyPost.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { SectionTitle } from "../../components/SectionTitle/SectionTitle";
+import { UpdateMyPostForm } from "../../components/UpdateMyPostForm/UpdateMyPostForm";
+import axios from "axios";
+import { useLoaderData } from "react-router-dom";
+import { LoadingSpin } from "../../components/LoadingSpin/LoadingSpin";
+
+export function UpdateMyPost() {
+  const [mypost, setMyPost] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const id = useLoaderData();
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/myposts/${id}`, {
+        withCredentials: true,
+      })
+      .then(({ data }) => {
+        setMyPost(data.message);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <SectionTitle title={"Update My Post"} size={3} />
+      {loading ? <LoadingSpin /> : <UpdateMyPostForm {...mypost} />}
+    </div>
+  );
+}
