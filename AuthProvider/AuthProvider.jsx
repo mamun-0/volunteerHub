0e31bf63b8770dba2b1fee3e67064c6bba45f5f6
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-08-08 12:53:32 +0600

    axios secure and implement jwt

--- AuthProvider/AuthProvider.jsx
+++ AuthProvider/AuthProvider.jsx
@@ -3,18 +3,28 @@ import { useFirebaseAuth } from "../hooks/useFirebaseAuth";
 import { useLoading } from "../hooks/useLoading";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../config/firebase";
+import { useAxiosSecure } from "../Axios/useAxiosSecure";
 export const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
   const firebaseAuth = useFirebaseAuth(null);
+  const secure = useAxiosSecure();
   const [loading, setLoading] = useLoading(true);
   useEffect(() => {
     const { setUser } = firebaseAuth;
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
       if (currentUser) {
         setUser(currentUser);
-      } else {
-        setUser(null);
+        secure.post("/jwt", { email: currentUser.email }).then((res) => {
+          const { token } = res.data;
+          console.log(currentUser);
+          if (token) {
+            window.localStorage.setItem("access-token", token);
+          } else {
+            window.localStorage.removeItem("access-token");
+          }
+        });
       }
       setLoading(false);
     });
